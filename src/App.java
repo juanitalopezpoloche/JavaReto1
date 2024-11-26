5e38a1e8d0058afef294b7bee29892044b045151
Juanita De Dios Lopez Poloche <juanitalopezpoloche.13@gmail.com>
2024-11-25 20:55:23 -0500

    Creación del menu - Juanita Lopez

--- src/App.java
+++ src/App.java
@@ -1,5 +1,48 @@
+import java.util.Scanner;
+
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        Scanner scanner = new Scanner(System.in);
+        int opcionMenu;
+        boolean mostrarMenuPrincipal = true;      
+        
+        while (mostrarMenuPrincipal) {
+
+            mostrarMenuPrincipal();
+            opcionMenu = scanner.nextInt();
+
+            switch (opcionMenu) {
+                case 1:
+                  
+                    break;
+
+                case 2:
+
+                    break;
+
+                case 3:
+
+                    break;
+
+                case 4:
+
+                   break;
+
+                default:
+                    System.out.println("\nOpción Inválida");
+            }
+
+        }
+
+        scanner.close();
+    }
+
+    public static void mostrarMenuPrincipal() {
+        System.out.println("\n------------  MENU  ------------");
+        System.out.println(" 1. Seleccionar un planeta destino");
+        System.out.println(" 2. Seleccionar una nave espacial");
+        System.out.println(" 3. Iniciar una simulación espacial");
+        System.out.println(" 4. Salir del programa");
+        System.out.println("\nDigite una opción (1-4):");
     }
 }
